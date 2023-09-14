9405612e7faab4541e3c23b350c1f3b8071cd1c4
Shocker <zacsmcdowell@gmail.com>
2023-09-14 02:51:37 -0400

    added to script.js, blank html & stylesheet.

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+
+// List of questions, answer choices, and correct answers
+var questions = [
+    {
+        title: 'How can a datatype be delcared to be a constant type?',
+        choices: ['const', 'var', 'let', 'constant'],
+        answer: 'const',
+    },
+    {
+        title: 'Arrays in JavaScript are defined by which of the following statements?',
+        choices: ['It is an ordered list of values', 'It is an ordered list of objects', 'It is an ordered list of string', 'It is an ordered list of functions'],
+        answer: 'It is an ordered list of values',
+    },
+    {
+        title: 'JavaScript ignores _______.',
+        choices: ['Newlines', 'Tabs', 'Spaces', 'All of the above'],
+        answer: 'All of the above',
+    },
+    {
+        title: 'Which JavaScript method is used to access an HTML element by id?',
+        choices: ['getElementById()', 'getElement(id)', 'getElementById(id)', 'elementById(id)'],
+        answer: 'getElementById(id)',
+    },
+    {
+        title: 'Which JavaScript method is used to write on the browser console?',
+        choices: ['console.write()', 'console.output()', 'console.log()', 'console.writeHTML()'],
+        answer: 'console.log()',
+    },
+    {
+        title: 'In JavaScript, multi-line comments start with __ and end with ___.',
+        choices: ['/* and */', '<!-- and -->', '## and ##', '// and //'],
+        answer: '/* and */',
+    },
+]
+
