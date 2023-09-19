49209e0a72da5adeb211f90eb703e2b0135ee438
Shocker <zacsmcdowell@gmail.com>
2023-09-18 22:22:04 -0400

    added timer and save highscore function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 var currentQuestionIndex = 0
 var score = 0
+var timer
+var timerCount = 30
 
+var timerEl = document.querySelector(".timer-count")
 var questionEl = document.getElementById('question')
 var choicesEl = document.getElementById('choices')
 var scoreEl = document.getElementById('score')
@@ -42,7 +45,11 @@ var questions = [
     },
 ]
 
+//Loads questions
 function loadQuestion() {
+
+    clearInterval(timer)
+
     var currentQuestion = questions[currentQuestionIndex]
     questionEl.textContent = currentQuestion.title
     choicesEl.innerHTML = ''
@@ -60,8 +67,30 @@ function loadQuestion() {
 
     questionNumberEl.textContent = currentQuestionIndex + 1
     totalQuestionsEl.textContent = questions.length
+    startTimer()
 }
 
+//Starts timer 
+function startTimer() {
+    timer = setInterval(function () {
+        timerCount--
+
+        // Update the timer display
+        var timerEl = document.querySelector('.timer-count')
+        timerEl.textContent = timerCount
+
+        // Check if time has run out
+        if (timerCount <= 0) {
+            // Clear interval and end the quiz
+            clearInterval(timer)
+            endQuiz()
+            timerEl.textContent = " "
+        }
+    }, 1000)
+}
+
+//Displays Correct or Incorrect for choice
+//Adds score if choice is Correct
 function checkAnswer(selectedChoice) {
     var currentQuestion = questions[currentQuestionIndex]
 
@@ -72,17 +101,19 @@ function checkAnswer(selectedChoice) {
         resultEl.textContent = 'Incorrect!'
     }
 
-    currentQuestionIndex++;
+    currentQuestionIndex++
 
     if (currentQuestionIndex < questions.length) {
         loadQuestion()
     } else {
-        // Quiz is finished
+        
         endQuiz()
     }
 }
 
+//Ends the quiz
 function endQuiz() {
+    timerEl.textContent = " "
     questionEl.textContent = 'Quiz Finished!'
     choicesEl.innerHTML = ''
     scoreEl.textContent = score + ' out of ' + questions.length
@@ -91,14 +122,43 @@ function endQuiz() {
         resultEl.style.display = "none"
     }
     document.getElementById('high-score-button').style.display = "block"
+
 }
 
 loadQuestion()
 
+//Displays High-scores on the right side of the High-score page
 function displayHighScores() {
-    document.getElementById('high-scores').style.display = 'block'
+    var highScores = JSON.parse(localStorage.getItem("highScores")) || []
+
+    var highScoresList = document.getElementById("high-scores-list")
+    highScoresList.innerHTML = "" // Clear the previous list
+
+    highScores.forEach((entry, index) => {
+        const listItem = document.createElement("li")
+        listItem.textContent = `${entry.initials}: ${entry.score}`
+        highScoresList.appendChild(listItem)
+    })
 }
 
+//Saves High-score to localStorage
+function saveHighScore(initials, score) {
+    var highScores = JSON.parse(localStorage.getItem('highScores')) || []
+    var newHighScore = { initials: initials, score: score }
+    highScores.push(newHighScore)
+    highScores.sort(function (a, b) {
+        return b.score - a.score
+    })
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+}
+
+// Event listener for starting the quiz
+document.getElementById('start-button').addEventListener('click', startQuiz)
+
+// Event listener for submitting a high score
 document.getElementById('submit-score').addEventListener('click', function () {
-    const initials = document.getElementById('initials').value
+    var initials = document.getElementById('initials').value
+    saveHighScore(initials, score)
+    displayHighScores()
 })
+
